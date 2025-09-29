e686ba4a3c591f997aaaa25f64d806f04b1bf8e0
paulyswen <paul51305040@gmail.com>
2025-09-29 15:47:12 +0800

    remove logger

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:funday_test/pages/home_screen.dart';
 
-base class Logger extends ProviderObserver {
-  @override
-  void didUpdateProvider(
-    ProviderObserverContext context,
-    Object? previousValue,
-    Object? newValue,
-  ) {
-    debugPrint('''
-{
-  "provider": "${context.provider.name ?? context.provider.runtimeType}",
-  "newValue": "$newValue"
-}''');
-  }
-}
-
 void main() {
   runApp(
     ProviderScope(
-      observers: [Logger()],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: const HomeScreen(),
